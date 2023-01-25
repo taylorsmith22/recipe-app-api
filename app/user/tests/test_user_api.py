39064ab277e41b6464fc179304735f8c97c779f7
taylorsmith22 <taylorsmith2202@outlook.com>
2023-01-25 12:08:55 -0500

    Fix errors

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -32,7 +32,7 @@ class PublicUserApiTests(TestCase):
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status_code, status.HTTP_202_CREATED)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
@@ -42,7 +42,7 @@ class PublicUserApiTests(TestCase):
         payload = {
             'email': 'test@example.com',
             'password': 'testpass123',
-            'name': 'Test Name',
+            'name': 'Test name',
         }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
--- app/user/views.py
+++ app/user/views.py
@@ -8,4 +8,4 @@ from user.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
-    serializers_class = UserSerializer
+    serializer_class = UserSerializer
